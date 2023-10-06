d6ff6c9c618e30c34d507b50677e0c3e5fd370a5
Diego Oyarzo <diegoyarzonavia@gmail.com>
2023-10-06 12:31:43 -0300

    ordencomprav1

--- tarea1/OrdenCompra.java
+++ tarea1/OrdenCompra.java
@@ -4,19 +4,35 @@ import java.util.Date;
 
 public class OrdenCompra {
     private Date fecha;
-    private String Estado;
+    private String estado;
     private Cliente cliente;
-    private ArrayList<ArrayList<DetalleOrden>> Detalles;
+    private ArrayList<DetalleOrden> losdetalles;
     public OrdenCompra(Date fecha, String estado, Cliente cliente){
-this.fecha=fecha;
-this.Estado=estado;
-this.cliente=cliente;
+        this.fecha=fecha;
+        this.estado=estado;
+        this.cliente=cliente;
     }
-
     //metodo para agregar Detalle de orden al array list
-    public void AgregaDetalle(){
-       // Detalles.add(Detalles);
+    public void AgregaDetalle(DetalleOrden detalle){
+       losdetalles.add(detalle);
     }
 
-
+    public float calcPrecio(){
+        float total = 0;
+        for (int i=0;i<losdetalles.size();i++ ){
+            DetalleOrden detalle = losdetalles.get(i);
+            total = total + detalle.calcPrecio();
+        }
+        return total;
+    }
+    /* hay que usar el calcPrecio para los 2 primeros
+    public float calcPrecioSinIVA(){
+        return (float) (cantidad * articulo.getPrecio() * 0.81);
+    }
+    public float calclIVA(){
+        return (float) (cantidad * articulo.getPrecio() * 0.19);
+    }
+    public float calcPeso(){
+        return (float) (cantidad * articulo.getPeso() * 0.81);
+    }*/
 }
