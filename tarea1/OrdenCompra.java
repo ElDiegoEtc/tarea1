0ce928fd718875d8787d2507f0bc674443279c2c
Bianca <bseminario2023@udec.cl>
2023-10-05 22:58:01 -0500

    relacion orden de compra y detalle orden

--- tarea1/OrdenCompra.java
+++ tarea1/OrdenCompra.java
@@ -1,8 +1,22 @@
 package tarea1 ;
-
+import java.util.ArrayList;
 import java.util.Date;
 
 public class OrdenCompra {
     private Date fecha;
+    private String Estado;
+    private Cliente cliente;
+    private ArrayList<ArrayList<DetalleOrden>> Detalles;
+    public OrdenCompra(Date fecha, String estado, Cliente cliente){
+this.fecha=fecha;
+this.Estado=estado;
+this.cliente=cliente;
+    }
+
+    //metodo para agregar Detalle de orden al array list
+    public void AgregaDetalle(){
+       // Detalles.add(Detalles);
+    }
+
 
 }
--- tarea1/main.java
+++ tarea1/main.java
@@ -4,7 +4,8 @@ public class main {
     public static void main(String[] args) {
         Articulo a1= new Articulo(18,"leche","rica en proteina",12);
         DetalleOrden D1= new DetalleOrden(a1,2);
-        System.out.println(D1.calclIVA());
+
+
 
     }
 }
