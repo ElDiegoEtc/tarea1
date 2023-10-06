f6ba9b8a6db9cb75e357ecdd76f186d35f8bbcca
Bianca <bseminario2023@udec.cl>
2023-10-06 12:07:06 -0500

    Metodo Arraylist para detalles compra

--- tarea1/OrdenCompra.java
+++ tarea1/OrdenCompra.java
@@ -2,20 +2,32 @@ package tarea1 ;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ *
+ */
 public class OrdenCompra {
     private Date fecha;
     private String Estado;
     private Cliente cliente;
-    private ArrayList<ArrayList<DetalleOrden>> Detalles;
+    private ArrayList<DetalleOrden> Detalles;
+
+    /**
+     *
+     * @param fecha
+     * @param estado
+     * @param cliente
+     */
     public OrdenCompra(Date fecha, String estado, Cliente cliente){
 this.fecha=fecha;
 this.Estado=estado;
 this.cliente=cliente;
+this.Detalles= new ArrayList<DetalleOrden>();
     }
 
     //metodo para agregar Detalle de orden al array list
-    public void AgregaDetalle(){
-       // Detalles.add(Detalles);
+    public void AgregaDetalle(DetalleOrden detalleOrden){
+        Detalles.add(detalleOrden);
+
     }
 
 
